2e62caa46720f8480c79a112d9bc72fd3b52cf42
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-11-12 23:21:27 +0100

    Add isOutOfBounds checking function

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,3 +1,7 @@
-function knightMoves(start, end){
-    
+const isOutOfBounds = (pos) =>
+  pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7;
+
+function knightMoves(start, end) {
+  if (isOutOfBounds(start) || isOutOfBounds(end)) {
+  }
 }
