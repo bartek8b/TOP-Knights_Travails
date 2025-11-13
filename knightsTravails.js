d3817fcb90c53165ada6806d8ffe1bb2229198aa
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-11-13 22:40:01 +0100

    Start working on knightMoves()

--- knightsTravails.js
+++ knightsTravails.js
@@ -1,13 +1,6 @@
 const isOutOfBounds = (pos) =>
   pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7;
 
-function knightMoves(start, end) {
-  if (isOutOfBounds(start) || isOutOfBounds(end)) {
-    console.error('Field coordinates must be in the range 0-7');
-    return null;
-  }
-}
-
 function possibleMoves(start) {
   const output = [];
 
@@ -29,4 +22,15 @@ function possibleMoves(start) {
   return output;
 }
 
-console.log(possibleMoves([0, 0]));
+function knightMoves(start, end) {
+  if (isOutOfBounds(start) || isOutOfBounds(end)) {
+    console.error('Field coordinates must be in the range 0-7');
+    return null;
+  }
+
+  const q = [start];
+  const visited = Array.from({ length: 8 }, () => Array(8).fill(false));
+  visited[start[0]][start[1]] = true;
+}
+
+// console.log(possibleMoves([0, 0]));
