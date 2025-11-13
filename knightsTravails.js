a9d51b50422fbee12c0c912214780773e9d5753f
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-11-13 20:26:50 +0100

    Add possibleMoves function

--- knightsTravails.js
+++ knightsTravails.js
@@ -3,5 +3,30 @@ const isOutOfBounds = (pos) =>
 
 function knightMoves(start, end) {
   if (isOutOfBounds(start) || isOutOfBounds(end)) {
+    console.error('Field coordinates must be in the range 0-7');
+    return null;
   }
 }
+
+function possibleMoves(start) {
+  const output = [];
+
+  const moves = [
+    [start[0] - 2, start[1] + 1],
+    [start[0] - 2, start[1] - 1],
+    [start[0] - 1, start[1] + 2],
+    [start[0] - 1, start[1] - 2],
+    [start[0] + 1, start[1] + 2],
+    [start[0] + 1, start[1] - 2],
+    [start[0] + 2, start[1] + 1],
+    [start[0] + 2, start[1] - 1],
+  ];
+
+  for (let move of moves) {
+    if (!isOutOfBounds(move)) output.push(move);
+  }
+
+  return output;
+}
+
+console.log(possibleMoves([0, 0]));
